e76062b86aa1d4b6e60bbbfa7b91f157139916c3
Brandon Linton <blinton@outlook.com>
2023-04-04 08:22:21 -0400

    Update and rename Install-OOBE.ps1 to Prepare-OOBE.ps1

---
+++ Prepare-OOBE.ps1
@@ -0,0 +1,52 @@
+$Global:Transcript = "$((Get-Date).ToString('yyyy-MM-dd-HHmmss'))-Prepare-OOBE.log"
+Start-Transcript -Path (Join-Path "$env:ProgramData\Microsoft\IntuneManagementExtension\Logs\OSD\" $Global:Transcript) -ErrorAction Ignore
+
+if ($env:SystemDrive -eq 'X:')
+{
+    Write-Warning 'This PSCloudScript cannot be run from WinPE'
+    Start-Sleep -Seconds 5
+}
+elseif ($env:UserName -ne 'defaultuser0')
+{
+    Write-Warning 'This PSCloudScript must be run from OOBE'
+    Start-Sleep -Seconds 5
+}
+else
+{
+    if ((Get-ExecutionPolicy) -ne 'RemoteSigned')
+    {
+        Write-Verbose -Verbose 'Set PowerShell ExecutionPolicy to RemoteSigned'
+        Set-ExecutionPolicy RemoteSigned -Force
+    }
+    if (-not (Get-PackageProvider | Where-Object {$_.Name -eq 'NuGet'}))
+    {
+        Write-Verbose -Verbose 'Install PackageProvider NuGet'
+        Install-PackageProvider -Name NuGet -Force
+    }
+
+    if ((Get-Module PowerShellGet).version -lt [System.Version]'2.2.5.0')
+    {
+        Write-Verbose -Verbose 'Install PowerShell Module PowerShellGet'
+        Install-Module PowerShellGet -Force
+    }
+    if ((Get-Module PackageManagement).version -lt [System.Version]'1.4.7.0')
+    {
+        Write-Verbose -Verbose 'Install PowerShell Module PackageManagement'
+        Install-Module PackageManagement -Force
+    }
+
+    Write-Verbose -Verbose 'Install PowerShell Module OSD'
+    Install-Module OSD -Force
+
+    Write-Verbose -Verbose 'Install PowerShell Module AutopilotOOBE'
+    Install-Module AutopilotOOBE -Force
+
+    
+    Write-Verbose -Verbose 'Install PowerShell Script Get-WindowsAutoPilotInfo'
+    Install-Script -Name Get-WindowsAutoPilotInfo -Force
+
+    Write-Verbose 'Run AutopilotOOBE in the new PowerShell window' -Verbose
+    Start-Sleep -Seconds 5
+    Start-Process PowerShell.exe -ArgumentList 
+}
+Stop-Transcript
