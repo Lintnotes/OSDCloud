83f47d6d9a9c748a589fcfb712985d080a170b4e
Brandon Linton <blinton@outlook.com>
2023-04-04 08:25:18 -0400

    Create Run-OOBE.ps1

---
+++ Run-OOBE.ps1
@@ -0,0 +1,4 @@
+$Global:Transcript = "$((Get-Date).ToString('yyyy-MM-dd-HHmmss'))-Run-OOBE.log"
+Start-Transcript -Path (Join-Path "$env:ProgramData\Microsoft\IntuneManagementExtension\Logs\OSD\" $Global:Transcript) -ErrorAction Ignore
+Start-OOBEDeploy -AddNetFX3 -UpdateDrivers -UpdateWindows -SetEdition Enterprise -RemoveAppx "Microsoft.549981C3F5F10","Microsoft.GetHelp","Microsoft.Getstarted","Microsoft.Microsoft3DViewer","Microsoft.MicrosoftOfficeHub","Microsoft.MicrosoftSolitaireCollection","Microsoft.MixedReality.Portal","Microsoft.People","Microsoft.SkypeApp","Microsoft.Wallet","microsoft.windowscommunicationsapps","Microsoft.WindowsFeedbackHub","Microsoft.Xbox.TCUI","Microsoft.XboxApp","Microsoft.XboxGameOverlay","Microsoft.XboxGamingOverlay","Microsoft.XboxIdentityProvider","Microsoft.XboxSpeechToTextOverlay","Microsoft.YourPhone","Microsoft.ZuneMusic","Microsoft.ZuneVideo"
+Stop-Transcript
