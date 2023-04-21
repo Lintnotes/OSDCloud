7e48f9ba6e72e6d84128390663d2984c047323e7
Brandon Linton <blinton@outlook.com>
2023-04-21 13:36:12 -0400

    Update Start-OSDeployment.ps1

--- Start-OSDeployment.ps1
+++ Start-OSDeployment.ps1
@@ -15,13 +15,13 @@ $ScriptName = 'Start-OSDeployment'
 $ScriptVersion = '23.4.17.1'
 
 #Determine the proper Windows environment
-if ($env:SystemDrive -eq 'X:') {$WindowsPhase = 'WinPE'}
+if ($env:SystemDrive -eq 'X:') { $WindowsPhase = 'WinPE' }
 else {
     $ImageState = (Get-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Setup\State' -ErrorAction Ignore).ImageState
-    if ($env:UserName -eq 'defaultuser0') {$WindowsPhase = 'OOBE'}
-    elseif ($ImageState -eq 'IMAGE_STATE_SPECIALIZE_RESEAL_TO_OOBE') {$WindowsPhase = 'Specialize'}
-    elseif ($ImageState -eq 'IMAGE_STATE_SPECIALIZE_RESEAL_TO_AUDIT') {$WindowsPhase = 'AuditMode'}
-    else {$WindowsPhase = 'Windows'}
+    if ($env:UserName -eq 'defaultuser0') { $WindowsPhase = 'OOBE' }
+    elseif ($ImageState -eq 'IMAGE_STATE_SPECIALIZE_RESEAL_TO_OOBE') { $WindowsPhase = 'Specialize' }
+    elseif ($ImageState -eq 'IMAGE_STATE_SPECIALIZE_RESEAL_TO_AUDIT') { $WindowsPhase = 'AuditMode' }
+    else { $WindowsPhase = 'Windows' }
 }
 #Finish initialization
 Write-Host -ForegroundColor DarkGray "$ScriptName $ScriptVersion $WindowsPhase"
@@ -32,36 +32,36 @@ Invoke-Expression -Command (Invoke-RestMethod -Uri functions.osdcloud.com)
 #=================================================
 #region WinPE
 if ($WindowsPhase -eq 'WinPE') {
-$TLS12Protocol = [System.Net.SecurityProtocolType] 'Ssl3 , Tls12'
-[System.Net.ServicePointManager]::SecurityProtocol = $TLS12Protocol
-Set-PSRepository -Name 'PSGallery' -InstallationPolicy Trusted
-if ((Get-MyComputerManufacturer) -match 'Dell') {
-Install-Module DellBIOSProvider -Force
-$DellProviderPath = Split-Path -Path (Get-Module -ListAvailable DellBiosProvider).Path
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/Lintnotes/OSDCloud/main/ExtraFiles/msvcp140.dll" -OutFile "$DellProviderPath\msvcp140.dll" -ErrorAction SilentlyContinue
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/Lintnotes/OSDCloud/main/ExtraFiles/vcruntime140.dll" -OutFile "$DellProviderPath\vcruntime140.dll" -ErrorAction SilentlyContinue
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/Lintnotes/OSDCloud/main/ExtraFiles/vcruntime140_1.dll" -OutFile "$DellProviderPath\vcruntime140_1.dll" -ErrorAction SilentlyContinue
-Import-Module DellBIOSProvider
-}
-if ((Get-MyComputerManufacturer) -match 'HP') {
-Install-Module -Name HPCMSL -AcceptLicense
-Install-Module -Name HPCMSL -Force -AcceptLicense
-}
+    $TLS12Protocol = [System.Net.SecurityProtocolType] 'Ssl3 , Tls12'
+    [System.Net.ServicePointManager]::SecurityProtocol = $TLS12Protocol
+    Set-PSRepository -Name 'PSGallery' -InstallationPolicy Trusted
+    if ((Get-MyComputerManufacturer) -match 'Dell') {
+        Install-Module DellBIOSProvider -Force
+        $DellProviderPath = Split-Path -Path (Get-Module -ListAvailable DellBiosProvider).Path
+        Invoke-WebRequest -Uri "https://raw.githubusercontent.com/Lintnotes/OSDCloud/main/ExtraFiles/msvcp140.dll" -OutFile "$DellProviderPath\msvcp140.dll" -ErrorAction SilentlyContinue
+        Invoke-WebRequest -Uri "https://raw.githubusercontent.com/Lintnotes/OSDCloud/main/ExtraFiles/vcruntime140.dll" -OutFile "$DellProviderPath\vcruntime140.dll" -ErrorAction SilentlyContinue
+        Invoke-WebRequest -Uri "https://raw.githubusercontent.com/Lintnotes/OSDCloud/main/ExtraFiles/vcruntime140_1.dll" -OutFile "$DellProviderPath\vcruntime140_1.dll" -ErrorAction SilentlyContinue
+        Import-Module DellBIOSProvider
+    }
+    if ((Get-MyComputerManufacturer) -match 'HP') {
+        Install-Module -Name HPCMSL -AcceptLicense
+        Install-Module -Name HPCMSL -Force -AcceptLicense
+    }
 
-if ((Get-MyComputerModel) -match 'Virtual|Vmware') {
-    Write-Host -ForegroundColor Green "Setting Display Resolution to 1600x"
-    Set-DisRes 1600
-}
+    if ((Get-MyComputerModel) -match 'Virtual|Vmware') {
+        Write-Host -ForegroundColor Green "Setting Display Resolution to 1600x"
+        Set-DisRes 1600
+    }
 
-If (Test-Path $env:WINDIR\Temp\OSDVarsFile.txt) {
-    $OSDVars = Get-Content -Raw -Path $env:WINDIR\Temp\OSDVarsFile.txt | ConvertFrom-StringData
-}
-$AssignedComputerName = $OSDVars.ComputerName
+    If (Test-Path $env:WINDIR\Temp\OSDVarsFile.txt) {
+        $OSDVars = Get-Content -Raw -Path $env:WINDIR\Temp\OSDVarsFile.txt | ConvertFrom-StringData
+    }
+    $AssignedComputerName = $OSDVars.ComputerName
 
-#================================================
-#   Autopilot Configs
-#================================================
-$Production = @'
+    #================================================
+    #   Autopilot Configs
+    #================================================
+    $Production = @'
 {
     "CloudAssignedDomainJoinMethod":  0,
     "CloudAssignedDeviceName": "INT%SERIAL%",
@@ -79,7 +79,7 @@ $Production = @'
 }
 '@
 
-$Kiosks = @'
+    $Kiosks = @'
 {
     "CloudAssignedTenantId":  "aa90fdc2-6a50-4f3d-b4bd-7fe753d268aa",
     "CloudAssignedDeviceName": "INT%SERIAL%",
@@ -96,56 +96,87 @@ $Kiosks = @'
     "CloudAssignedTenantDomain":  "hubspotcorp.onmicrosoft.com"
 }
 '@
-#================================================
-#   [OS] Start-OSDCloud with Params
-#================================================
-$Params = @{
-    OSVersion = $OSDVars.OperatingSystem
-    OSBuild = "22H2"
-    OSEdition = "Enterprise"
-    OSLanguage = $OSDVars.OperatingSystemLanguage
-    OSLicense = "Retail"
-    SkipAutopilot = $True
-    SkipODT = $true
-    Firmware = $True
-    ZTI = $True
-}
-Start-OSDCloud @Params
-Start-EjectCD
-If($OSDVars.AutoPilotConfig -eq 'Hubspot Production Devices'){
-    $Production.Replace('"INT%SERIAL%"',$AssignedComputerName) | Out-File C:\Windows\Provisioning\AutoPilot\AutoPilotConfigurationFile.json | Out-Null
-}Else{
-    $Kiosks.Replace('"INT%SERIAL%"',$AssignedComputerName) | Out-File C:\Windows\Provisioning\AutoPilot\AutoPilotConfigurationFile.json | Out-Null
-}
+    #================================================
+    #   [OS] Start-OSDCloud with Params
+    #================================================
+    $Params = @{
+        OSVersion     = $OSDVars.OperatingSystem
+        OSBuild       = "22H2"
+        OSEdition     = "Enterprise"
+        OSLanguage    = $OSDVars.OperatingSystemLanguage
+        OSLicense     = "Retail"
+        SkipAutopilot = $True
+        SkipODT       = $true
+        Firmware      = $True
+        ZTI           = $True
+    }
 
-#================================================
-#  [PostOS] AutopilotOOBE CMD Command Line
-#================================================
-Write-Host -ForegroundColor Green "Create C:\Windows\Setup\Scripts\OOBE.cmd"
-$OOBECMD = @'
+    #=================================================
+    #   oobeCloud Settings
+    #=================================================
+    $Global:oobeCloud = @{
+        oobeSetDisplay               = $false
+        oobeSetRegionLanguage        = $false
+        oobeSetDateTime              = $false
+        oobeRegisterAutopilot        = $true
+        oobeRegisterAutopilotCommand = 'Get-WindowsAutopilotInfo -Online -GroupTag "Kiosk-Zoom" -Assign'
+        oobeRemoveAppxPackage        = $false
+        oobeRemoveAppxPackageName    = 'CommunicationsApps', 'OfficeHub', 'People', 'Skype', 'Solitaire', 'Xbox', 'ZuneMusic', 'ZuneVideo'
+        oobeAddCapability            = $false
+        oobeAddCapabilityName        = 'ActiveDirectory', 'BitLocker', 'GroupPolicy', 'RemoteDesktop', 'ServerManager', 'VolumeActivation', 'NetFX'
+        oobeUpdateDrivers            = $true
+        oobeUpdateWindows            = $true
+        oobeRestartComputer          = $true
+        oobeStopComputer             = $false
+    }
+    Start-OSDCloud @Params
+    Start-EjectCD
+    If ($OSDVars.AutoPilotConfig -eq 'Hubspot Production Devices') {
+        $AutoPilotConfig = $Production.Replace('"INT%SERIAL%"', $AssignedComputerName)
+    }
+    Else {
+        If ($OSDVars.AutoPilotConfig -match 'zoom'){
+            $AutoPilotConfig = $Kiosks.Replace('"INT%SERIAL%"', $AssignedComputerName)
+            $AutoPilotConfig = $Kiosks.Replace('"9d255231-51ea-4353-8f73-bc7f3b403727"', "Zoom-9d255231-51ea-4353-8f73-bc7f3b403727") 
+        }
+        elseif ($OSDVars.AutoPilotConfig -match 'Appspace') {
+            $AutoPilotConfig = $Kiosks.Replace('"INT%SERIAL%"', $AssignedComputerName)
+            $AutoPilotConfig = $Kiosks.Replace('"9d255231-51ea-4353-8f73-bc7f3b403727"', "Appspace-9d255231-51ea-4353-8f73-bc7f3b403727") 
+        }
+    }
+    If($Null -ne $AutoPilotConfig){
+        $AutoPilotConfig  | Out-File C:\Windows\Provisioning\AutoPilot\AutoPilotConfigurationFile.json | Out-Null
+    }
+    
+    #================================================
+    #  [PostOS] AutopilotOOBE CMD Command Line
+    #================================================
+    Write-Host -ForegroundColor Green "Create C:\Windows\Setup\Scripts\OOBE.cmd"
+    $OOBECMD = @'
 PowerShell -NoL -Com Set-ExecutionPolicy RemoteSigned -Force
 Set Path = %PATH%;C:\Program Files\WindowsPowerShell\Scripts
 Start /Wait PowerShell -NoL -C Invoke-WebPSScript https://raw.githubusercontent.com/Lintnotes/OSDCloud/main/Install-EmbeddedProductKey.ps1
 Start /Wait PowerShell -NoL -C Invoke-WebPSScript https://raw.githubusercontent.com/Lintnotes/OSDCloud/main/Cleanup-Script.ps1
+Start /Wait Powershell iex(irm sandbox.osdcloud.com)
 Start /Wait PowerShell -NoL -C Restart-Computer -Force
 '@
-$OOBECMD | Out-File -FilePath 'C:\Windows\Setup\Scripts\OOBE.cmd' -Encoding ascii -Force
+    $OOBECMD | Out-File -FilePath 'C:\Windows\Setup\Scripts\OOBE.cmd' -Encoding ascii -Force
 
-#================================================
-#  [PostOS] SetupComplete CMD Command Line
-#================================================
-Write-Host -ForegroundColor Green "Create C:\Windows\Setup\Scripts\SetupComplete.cmd"
-$SetupCompleteCMD = @'
+    #================================================
+    #  [PostOS] SetupComplete CMD Command Line
+    #================================================
+    Write-Host -ForegroundColor Green "Create C:\Windows\Setup\Scripts\SetupComplete.cmd"
+    $SetupCompleteCMD = @'
 '@
-$SetupCompleteCMD | Out-File -FilePath 'C:\Windows\Setup\Scripts\SetupComplete.cmd' -Encoding ascii -Force
+    $SetupCompleteCMD | Out-File -FilePath 'C:\Windows\Setup\Scripts\SetupComplete.cmd' -Encoding ascii -Force
 
-#=======================================================================
-#   Restart-Computer
-#=======================================================================
-#Write-Host "Restarting in 10 seconds!" -ForegroundColor Green
-Start-Sleep -Seconds 5
-$null = Stop-Transcript -ErrorAction Ignore
-wpeutil reboot
+    #=======================================================================
+    #   Restart-Computer
+    #=======================================================================
+    #Write-Host "Restarting in 10 seconds!" -ForegroundColor Green
+    Start-Sleep -Seconds 5
+    $null = Stop-Transcript -ErrorAction Ignore
+    wpeutil reboot
 }
 #endregion
 #=================================================
@@ -177,7 +208,7 @@ if ($WindowsPhase -eq 'OOBE') {
     #If not, need to register the device using the Enterprise GroupTag and Assign it
     elseif ($TestAutopilotProfile -eq $false) {
         $AutopilotRegisterCommand = 'Get-WindowsAutopilotInfo -Online -GroupTag Enterprise -Assign'
-        $AutopilotRegisterProcess = osdcloud-AutopilotRegisterCommand -Command $AutopilotRegisterCommand;Start-Sleep -Seconds 30
+        $AutopilotRegisterProcess = osdcloud-AutopilotRegisterCommand -Command $AutopilotRegisterCommand; Start-Sleep -Seconds 30
     }
     #Or maybe we just can't figure it out
     else {
