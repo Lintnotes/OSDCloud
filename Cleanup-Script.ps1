bfbed68fe2cf9d52d0bfb38981e9c8ba946a077a
Brandon Linton <blinton@outlook.com>
2023-04-25 12:33:05 -0400

    Update Cleanup-Script.ps1

--- Cleanup-Script.ps1
+++ Cleanup-Script.ps1
@@ -1,20 +1,11 @@
 $Global:Transcript = "$((Get-Date).ToString('yyyy-MM-dd-HHmmss'))-Cleanup-Script.log"
-Start-Transcript -Path (Join-Path "$env:ProgramData\Microsoft\IntuneManagementExtension\Logs\OSD\" $Global:Transcript) -ErrorAction Ignore
+Start-Transcript -Path (Join-Path "$env:WINDIR\Temp\" $Global:Transcript) -ErrorAction Ignore
 
 Write-Host "Execute OSD Cloud Cleanup Script" -ForegroundColor Green
 
-# Copying the OOBEDeploy and AutopilotOOBE Logs
-Get-ChildItem 'C:\Windows\Temp' -Filter *OOBE* | Copy-Item -Destination 'C:\ProgramData\Microsoft\IntuneManagementExtension\Logs\OSD' -Recurse -Force
-
 # Copying OSDCloud Logs
 If (Test-Path -Path 'C:\OSDCloud\Logs') {
-    Move-Item 'C:\OSDCloud\Logs\*.*' -Destination 'C:\ProgramData\Microsoft\IntuneManagementExtension\Logs\OSD' -Recurse -Force
-}
-Move-Item 'C:\ProgramData\OSDeploy\*.*' -Destination 'C:\ProgramData\Microsoft\IntuneManagementExtension\Logs\OSD' -Recurse -Force
-
-If (Test-Path -Path 'C:\Temp') {
-    Get-ChildItem 'C:\Temp' -Filter *OOBE* | Copy-Item -Destination 'C:\ProgramData\Microsoft\IntuneManagementExtension\Logs\OSD' -Recurse -Force
-    Get-ChildItem 'C:\Windows\Temp' -Filter *Events* | Copy-Item -Destination 'C:\ProgramData\Microsoft\IntuneManagementExtension\Logs\OSD' -Recurse -Force
+    Move-Item 'C:\OSDCloud\Logs\*.*' -Destination 'C:\ProgramData\Microsoft\IntuneManagementExtension\Logs\OSD' -Force
 }
 
 # Cleanup directories
@@ -59,9 +50,8 @@ $Process.ExitCode
 
 If($env:Computername -match 'Zoom|Sign'){
     $RegKeyPath = "HKLM:\SOFTWARE\Policies\Microsoft\PassportForWork"
+    If(!(Test-Path $RegKeyPath)){New-Item $RegKeyPath | Out-Null}
     New-ItemProperty -Path $RegKeyPath -Name "Enabled" -Value "0" -PropertyType DWORD -Force | Out-Null
 }
-
-
 
 Stop-Transcript
