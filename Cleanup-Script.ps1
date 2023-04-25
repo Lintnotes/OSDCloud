c1cdef79043fc439cf8813cf73bb5eba74babbee
Brandon Linton <blinton@outlook.com>
2023-04-25 11:59:05 -0400

    Update Cleanup-Script.ps1

--- Cleanup-Script.ps1
+++ Cleanup-Script.ps1
@@ -22,4 +22,46 @@ If (Test-Path -Path 'C:\OSDCloud') { Remove-Item -Path 'C:\OSDCloud' -Recurse -F
 If (Test-Path -Path 'C:\Drivers') { Remove-Item 'C:\Drivers' -Recurse -Force }
 If (Test-Path -Path 'C:\Temp') { Remove-Item 'C:\Temp' -Recurse -Force }
 
+$EnrollmentID = ((Get-Childitem 'HKLM:\SOFTWARE\Microsoft\Provisioning\OMADM\Accounts\').Name).Split('\')[-1]
+
+$DeviceLockKeys = @(
+'HKLM:\SYSTEM\CurrentControlSet\Control\EAS'
+"HKLM:\SOFTWARE\Microsoft\PolicyManager\Providers\$EnrollmentID\default\Device\DeviceLock"
+'HKLM:\SOFTWARE\Microsoft\PolicyManager\current\device\DeviceLock'
+)
+
+ForEach ($Key in $DeviceLockKeys) {
+    Remove-Item $Key -Force -ErrorAction Continue
+}
+
+$Inf = @"
+[Unicode]
+Unicode=yes
+[Version]
+signature="`$CHICAGO`$"
+Revision=1
+[System Access]
+MinimumPasswordAge = 0
+MaximumPasswordAge = 720
+MinimumPasswordLength = 12
+PasswordComplexity = 1
+PasswordHistorySize = 24
+LockoutBadCount = 10
+ResetLockoutCount = 5
+LockoutDuration = 5
+RequireLogonToChangePassword = 0
+ForceLogoffWhenHourExpire = 0
+"@
+ 
+$Null = New-Item -Path "$Env:SystemRoot\security\database" -Name SecPolicy.inf -ItemType File -Value $Inf -Force
+$Process = Start-Process -FilePath Secedit.exe -ArgumentList "/configure /db secedit.sdb /cfg $env:SystemRoot\Security\Database\SecPolicy.inf /overwrite /quiet" -NoNewWindow -PassThru -Wait
+$Process.ExitCode
+
+If($env:Computername -match 'Zoom|Sign'){
+    $RegKeyPath = "HKLM:\SOFTWARE\Policies\Microsoft\PassportForWork"
+    New-ItemProperty -Path $RegKeyPath -Name "Enabled" -Value "0" -PropertyType DWORD -Force | Out-Null
+}
+
+
+
 Stop-Transcript
