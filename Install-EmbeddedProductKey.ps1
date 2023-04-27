acef80abf4edbf0348a976132c50536590a8b2bb
Brandon Linton <blinton@outlook.com>
2023-04-27 12:00:43 -0400

    Update Install-EmbeddedProductKey.ps1

--- Install-EmbeddedProductKey.ps1
+++ Install-EmbeddedProductKey.ps1
@@ -1,21 +1,26 @@
 $Global:Transcript = "$((Get-Date).ToString('yyyy-MM-dd-HHmmss'))-Install-EmbeddedProductKey.log"
 Start-Transcript -Path (Join-Path "$env:ProgramData\Microsoft\IntuneManagementExtension\Logs\OSD\" $Global:Transcript) -ErrorAction Ignore
 
-Write-Host -ForegroundColor Green "Get embedded product key"
-$Key = (Get-WmiObject SoftwareLicensingService).OA3xOriginalProductKey
-
-If ($Key) {
-    Write-Host -ForegroundColor Green "Installing embedded product key"
-    Invoke-Command -ScriptBlock {& 'cscript.exe' "$env:windir\system32\slmgr.vbs" '/ipk' "$($Key)"}
-    Start-Sleep -Seconds 5
-
-    Write-Host -ForegroundColor Green "Activating embedded product key"
-    Invoke-Command -ScriptBlock {& 'cscript.exe' "$env:windir\system32\slmgr.vbs" '/ato'}
-    Start-Sleep -Seconds 5
+$Model = (Get-WmiObject -Class Win32_ComputerSystem).Model
+if ($Model -match 'Virtual|Vmware') {
+        Write-Host -ForegroundColor Green "Skipping VM"
 }
+Else{
+    Write-Host -ForegroundColor Green "Get embedded product key"
+    $Key = (Get-WmiObject SoftwareLicensingService).OA3xOriginalProductKey
 
-Else {
-    Write-Host -ForegroundColor Red 'No embedded product key found.'
+    If ($Key) {
+        Write-Host -ForegroundColor Green "Installing embedded product key"
+        Invoke-Command -ScriptBlock {& 'cscript.exe' "$env:windir\system32\slmgr.vbs" '/ipk' "$($Key)"}
+        Start-Sleep -Seconds 5
+
+        Write-Host -ForegroundColor Green "Activating embedded product key"
+        Invoke-Command -ScriptBlock {& 'cscript.exe' "$env:windir\system32\slmgr.vbs" '/ato'}
+        Start-Sleep -Seconds 5
+    }
+
+    Else {
+        Write-Host -ForegroundColor Red 'No embedded product key found.'
+    }
 }
-
 Stop-Transcript
